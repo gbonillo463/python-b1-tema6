bd3368ba02d439ae2e688db150020f147db9551b
gbonillo463 <spamgerard4@gmail.com>
2026-01-26 22:25:42 +0100

    ej 6c1 COMPLETADO

--- 6c/ej6c1.py
+++ 6c/ej6c1.py
@@ -30,39 +30,6 @@ Ejemplo:
 
     Salida:
         "Negative"
-
-Enunciat:
-Utilitzant la llibreria NLTK, implementa una funció 'sentiment_analysis(text)'
-que rep com a paràmetre 'text' que és una cadena de strings, la mateixa que
-ha de ser analitzada per saber si aquesta frase representa una emoció negativa o
-positiva.
-
-S'ha d'utilitzar 'SentimentIntensityAnalyzer' que és importada des de
-'nltk.sentiment', d'aquesta manera obtindrem una puntuació sobre la base del número
-de paraules que s'estiguin associant a alguna cosa positiva o negativa dins la frase
-analitzada.
-
-Utilitzarem el mòdul nltk.sentiment.vader per a analitzar el sentiment d'un text.
-Et recmendamos que revisis la documentació:
-https://www.nltk.org/api/nltk.sentiment.vader.html
-
-En concret, revisa la funció polarity_scores(text)
-
-Si la puntuació és més gran de 0.05 es retorna un resultat positiu, si és menor
-a -0.05 es retorna un resultat negatiu i si es troba entre els valors
--0.05 i 0.05 inclusives, es retorna un resultat neutre.
-
-Paràmetre:
-text: Cadena de strings.
-
-Exemple:
-     Entrada:
-         "I love going to the beach on a sunny day, but I hate getting
-         sunburned."
-
-     Sortida:
-         "Negative"
-
 """
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -70,12 +37,19 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 nltk.download("vader_lexicon")
 
 def sentiment_analysis(text):
-    #Write your code here
-    pass
+    sia = SentimentIntensityAnalyzer()
+    score = sia.polarity_scores(text)
+
+    if score['compound'] >= 0.05:
+        return 'Positive'
+    if score['compound'] <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# text = "This product is okay."
-# print(sentiment_analysis(text))
+text = "This product is okay."
+print(sentiment_analysis(text))
